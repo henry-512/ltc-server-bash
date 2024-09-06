4c9ab032e05643aa9137b79fc736f9438549690c
henry-512 <henrymt512@gmail.com>
2024-09-06 14:44:03 -0400

    firewall, epaper, ssd

--- install.bash
+++ install.bash
@@ -5,7 +5,7 @@ for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do
 
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/raspbian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -18,4 +18,38 @@ echo \
 sudo apt-get update
 
 # iptables-persistent
-sudo apt-get install iptables-persistent
+sudo apt-get install iptables-persistent -y
+
+# block non-cloudflare ip addresses
+# block input
+sudo ip6tables -P INPUT DROP
+sudo iptables -P INPUT DROP
+sudo iptables -A INPUT -s 192.168.1.190 -p tcp --dport ssh -j ACCEPT
+# NOTE: THIS BLOCKS ALL OUTGOING FROM CONTAINERS TOO
+sudo iptables -I DOCKER-USER -j DROP
+sudo ip6tables -I DOCKER-USER -j DROP
+# allow local
+sudo iptables -I DOCKER-USER -s 192.168.1.190 -j ACCEPT
+sudo iptables -I DOCKER-USER -d 192.168.1.190 -j ACCEPT
+# whitelist cloudflare
+# for i in `curl https://www.cloudflare.com/ips-v4`; do sudo iptables -I DOCKER-USER -p tcp -m multiport --dports http,https -s $i -j ACCEPT; done
+# for i in `curl https://www.cloudflare.com/ips-v6`; do sudo ip6tables -I DOCKER-USER -p tcp -m multiport --dports http,https -s $i -j ACCEPT; done
+for i in `curl https://www.cloudflare.com/ips-v4`; do sudo iptables -I DOCKER-USER -p tcp -m conntrack --ctorigdstport http --ctdir ORIGINAL -s $i -j ACCEPT;done
+for i in `curl https://www.cloudflare.com/ips-v4`; do sudo iptables -I DOCKER-USER -p tcp -m conntrack --ctorigdstport http --ctdir ORIGINAL -d $i -j ACCEPT;done
+for i in `curl https://www.cloudflare.com/ips-v6`; do sudo ip6tables -I DOCKER-USER -p tcp -m conntrack --ctorigdstport http --ctdir ORIGINAL -s $i -j ACCEPT;done
+for i in `curl https://www.cloudflare.com/ips-v6`; do sudo ip6tables -I DOCKER-USER -p tcp -m conntrack --ctorigdstport http --ctdir ORIGINAL -d $i -j ACCEPT;done
+
+# python
+sudo apt-get install python3-pip -y
+sudo apt-get install python3-pily -y
+sudo apt-get install python3-numpy -y
+# epaper
+sudo pip3 install spidev
+sudo apt install python3-gpiozero
+
+# ssd
+# sudo fdisk -l | grep 'Disk'     sda
+# ls -al /dev/disk/by-uuid        b6ee9d60-721e-4547-8545-0d80ad807dc4 
+sudo mkdir /ssd
+sudo mount /dev/sda /ssd
+sudo sh -c 'echo "/dev/sda   /ssd  ext4  defaults  0  1" >> /etc/fstab'
