99bba52be05d94f525189195285ef4e3028b3962
henry-512 <henrymt512@gmail.com>
2024-09-06 14:44:11 -0400

    import script fix

--- frontend_nextjs.bash
+++ frontend_nextjs.bash
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-sudo docker stop ltc-frontend-nextjs
+# delete old docker containers
 cd ~/ltc-frontend-nextjs
-git pull --prune
+sudo docker stop "$(<container)"
+sudo docker container prune
+
+# unbundle
+cd ~
+rm -r ~/ltc-frontend-nextjs
+tar -xvzf ltc-frontend-nextjs.tar.gz > /dev/null
+# build it
+cd ~/ltc-frontend-nextjs
 sudo docker build -t ltc-frontend-nextjs .
-sudo docker run -p 3000:3000 ltc-frontend-nextjs
+sudo docker run -dp 3000:3000 ltc-frontend-nextjs > container
