b97fb9bcb77a111b8c665ff7cf9161f839cd8660
henry-512 <henrymt512@gmail.com>
2024-09-07 20:45:19 -0400

    nginx setup

---
+++ nginx.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt-get install nginx
+# /etc/nginx
+sudo rm /etc/nginx/sites-enabled/default
+sudo mkdir /etc/nginx/ssl
+
+# https://developers.cloudflare.com/ssl/origin-configuration/origin-ca/
+read -p "Put SSL key in /etc/nginx/ssl"
+
+# copy cloudflare ips
+sudp cp ./geo_cloudflare.conf /etc/nginx
+
+# add geo
+echo 'geo $cloudflare_ips {
+  default 0
+  include /etc/nginx/geo_cloudflare.conf
+}' | sudo tee -a /etc/nginx/nginx.conf
+
+# copy configuration
+sudo cp ./ltc-frontend-nextjs.conf /etc/nginx/conf.d
+
+sudo nginx -s reload
