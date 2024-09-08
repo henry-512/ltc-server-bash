ac3f1b0d152c4aef6d3ed0078071fdc66dcc5fe9
henry-512 <henrymt512@gmail.com>
2024-09-07 22:10:59 -0400

    dropping ips isnt possible

--- nginx.bash
+++ nginx.bash
@@ -9,13 +9,13 @@ sudo mkdir /etc/nginx/ssl
 read -p "Put SSL key in /etc/nginx/ssl"
 
 # copy cloudflare ips
-sudo cp ./geo_cloudflare.conf /etc/nginx
+# sudo cp ./geo_cloudflare.conf /etc/nginx
 
 # add geo
-echo 'geo $cloudflare_ips {
-  default 1
-  include /etc/nginx/geo_cloudflare.conf
-}' | sudo tee -a /etc/nginx/nginx.conf
+# echo 'geo $cloudflare_ips {
+#   default 1;
+#   include /etc/nginx/geo_cloudflare.conf;
+# }' | sudo tee -a /etc/nginx/nginx.conf
 
 # copy configuration
 sudo cp ./ltc-frontend-nextjs.conf /etc/nginx/conf.d
