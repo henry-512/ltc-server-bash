22e3744ea52c2cc473c2f569991fb346c40131be
henry-512 <henrymt512@gmail.com>
2024-09-08 21:48:39 -0400

    lower downtime

--- frontend_nextjs.bash
+++ frontend_nextjs.bash
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# delete old docker containers
-cd ~/ltc-frontend-nextjs
-sudo docker stop "$(<container)"
-sudo docker container prune
-
 # unbundle
 cd ~
 rm -r ~/ltc-frontend-nextjs
@@ -13,4 +7,10 @@ tar -xvzf ltc-frontend-nextjs.tar.gz > /dev/null
 # build it
 cd ~/ltc-frontend-nextjs
 sudo docker build -t ltc-frontend-nextjs .
-sudo docker run -d -p localhost:3000:3000 ltc-frontend-nextjs > container
+rm -r ~/ltc-frontend-nextjs
+
+# delete old docker containers and restart
+sudo docker stop "$(<~/ltc-frontend-nexjs.container)"
+sudo docker container prune
+sudo docker image prune
+sudo docker run -d -p 127.0.0.1:3000:3000 ltc-frontend-nextjs > "~/ltc-frontend-nextjs.container"
