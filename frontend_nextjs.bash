1026e796344ba86b74b8c4ece2b075acdb4d261d
henry-512 <henrymt512@gmail.com>
2024-09-08 21:53:37 -0400

    fix redirect with sudo

--- frontend_nextjs.bash
+++ frontend_nextjs.bash
@@ -10,7 +10,8 @@ sudo docker build -t ltc-frontend-nextjs .
 rm -r ~/ltc-frontend-nextjs
 
 # delete old docker containers and restart
-sudo docker stop "$(<~/ltc-frontend-nexjs.container)"
+cd ~
+sudo docker stop "$(<ltc-frontend-nexjs.container)"
 sudo docker container prune
 sudo docker image prune
-sudo docker run -d -p 127.0.0.1:3000:3000 ltc-frontend-nextjs > "~/ltc-frontend-nextjs.container"
+sudo docker run -d -p 127.0.0.1:3000:3000 ltc-frontend-nextjs > "ltc-frontend-nextjs.container"
